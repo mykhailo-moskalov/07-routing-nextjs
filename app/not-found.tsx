9184281765788dd5a6e805b5908f469f8779c40a
mishokkartoshki <misha@moskalov.com>
2025-10-04 09:18:35 +0200

    d comms

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,6 @@ const NotFound = () => {
   const router = useRouter();
 
   useEffect(() => {
-    // Редірект через 3 секунди
     const timer = setTimeout(() => router.push("/"), 3000);
     return () => clearTimeout(timer);
   }, [router]);
